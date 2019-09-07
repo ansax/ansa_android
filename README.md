e6ce3ff12e86c3a2a1a48237530b5029ba9b1dd0
tawenga <taveed@yandex.com>
2019-09-07 15:02:23 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ansa_android
---
+++ app/src/main/java/com/ansa/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.ansa;
+
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+
+public class LoginActivity extends ActivityCompat {
+}
