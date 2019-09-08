2a61c58a852657c0e2c9647991c47b6ba44a8023
tawenga <taveed@yandex.com>
2019-09-08 12:40:17 +0300

    Created login view

--- app/src/main/java/com/ansa/LoginActivity.java
+++ app/src/main/java/com/ansa/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.ansa;
 
-import android.app.Activity;
-import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class LoginActivity extends ActivityCompat {
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
