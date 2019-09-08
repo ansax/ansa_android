b798741c6c81d3381a5d94a94c1e1dc03f43089d
tawenga <taveed@yandex.com>
2019-09-08 17:09:49 +0300

    Created new ad layout

--- app/src/main/java/com/ansa/NewAdActivity.java
+++ app/src/main/java/com/ansa/NewAdActivity.java
@@ -3,11 +3,11 @@ package com.ansa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class NewAdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_new_ad);
     }
 }
